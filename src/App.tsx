07d87d52b7c1121ed2a7f9baf01ed9fddec221b1
G2o64 <104470190+G2o64@users.noreply.github.com>
2025-01-25 11:25:09 +0100

    feat: Add core application files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TerminalPortfolio from './components/Terminal/TerminalPortfolio';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <TerminalPortfolio />
+    </div>
+  );
+};
+
+export default App;
